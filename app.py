a174ad285c4f744fa5e86102fc8bbf85c0c31455
dubhunter <get@willmason.me>
2013-10-01 00:52:49 -0700

    fix spacing

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ time.sleep(3)
 last = ''
 
 while 1:
-  r = requests.get('http://sign.willmason.me/message.txt')
-  if r.status_code == 200 and last != r.text:
-    sio.write(r.text)
-    last = r.text
-  time.sleep(1)  
+    r = requests.get('http://sign.willmason.me/message.txt')
+    if r.status_code == 200 and last != r.text:
+        sio.write(r.text)
+        last = r.text
+    time.sleep(1)
