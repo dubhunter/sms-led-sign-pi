246f4c2678af118da7e22f54c79b56002ee3400e
dubhunter <get@willmason.me>
2013-10-02 09:15:00 -0700

    try to catch request exceptions

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ time.sleep(3)
 last = ''
 
 while 1:
-    r = requests.get('http://sign.willmason.me/message.txt')
+    try:
+        r = requests.get('http://sign.willmason.me/message.txt')
+    except Exception:
+        pass
     if r.status_code == 200 and last != r.text:
         sio.write(r.text)
         last = r.text
